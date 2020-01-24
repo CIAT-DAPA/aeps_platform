0802de7fd13813eb9a7eb54d6ab9d13c4a010397
stevensotelo <stevenbetancurt@hotmail.com>
2020-01-24 16:11:25 -0500

    in the users module, we added the option to register new user

--- src/AEPS/CIAT.DAPA.AEPS.WebAdministrative/Controllers/AccountController.cs
+++ src/AEPS/CIAT.DAPA.AEPS.WebAdministrative/Controllers/AccountController.cs
@@ -120,9 +120,12 @@ namespace CIAT.DAPA.AEPS.WebAdministrative.Controllers
                     //var callbackUrl = Url.EmailConfirmationLink(user.Id, code, Request.Scheme);
                     //await _emailSender.SendEmailConfirmationAsync(model.Email, callbackUrl);
 
-                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    //await _signInManager.SignInAsync(user, isPersistent: false);
                     _logger.LogInformation("User created a new account with password.");
-                    return RedirectToLocal(returnUrl);
+                    if (!string.IsNullOrEmpty(returnUrl))
+                        return RedirectToLocal(returnUrl);
+                    else
+                        return RedirectToAction("Index", "Users");
                 }
                 AddErrors(result);
             }
